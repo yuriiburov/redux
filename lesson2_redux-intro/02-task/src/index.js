a392061d2a09ccd3e22010f5bf636ef7afab29b4
yuriiburov <burovyuriireact@gmail.com>
2022-02-17 09:28:58 +0200

    init

--- lesson2_redux-intro/02-task/src/index.js
+++ lesson2_redux-intro/02-task/src/index.js
@@ -1,10 +1,10 @@
 import './index.scss';
-import store, { increment, decrement, reset } from './store';
+import store, { reset, decrement, increment } from './store';
 
 const resultElem = document.querySelector('.counter__result');
+const decrementBtn = document.querySelector('[data-action = "decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
 const resetBtn = document.querySelector('[data-action="reset"]');
-const decrementBtn = document.querySelector('[data-action="decrement"]');
 
 const onIncrement = () => {
   store.dispatch(increment());
@@ -25,10 +25,10 @@ resetBtn.addEventListener('click', onReset);
 store.subscribe(() => {
   const state = store.getState();
   const currentValue = state.history.reduce(
-    (acc, value) => Number(acc) + Number(value),
+    (acc, value) => acc + Number(value),
     0
   );
-  const historyString = state.history.join(' ');
+  const historyString = state.history.join('');
   resultElem.textContent =
     state.history.length === 0 ? '' : `${historyString} = ${currentValue}`;
 });
--- lesson2_redux-intro/02-task/src/store.js
+++ lesson2_redux-intro/02-task/src/store.js
@@ -4,23 +4,15 @@ const INCREMENT = 'COUNTER/INCREMENT';
 const DECREMENT = 'COUNTER/DECREMENT';
 const RESET = 'COUNTER/RESET';
 
-export const increment = () => {
-  return {
-    type: INCREMENT,
-  };
-};
-
-export const decrement = () => {
-  return {
-    type: DECREMENT,
-  };
-};
-
-export const reset = () => {
-  return {
-    type: RESET,
-  };
-};
+export const increment = () => ({
+  type: INCREMENT,
+});
+export const decrement = () => ({
+  type: DECREMENT,
+});
+export const reset = () => ({
+  type: RESET,
+});
 
 const initialState = {
   history: [],
