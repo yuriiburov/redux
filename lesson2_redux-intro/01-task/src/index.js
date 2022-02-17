644ee8df2532f4b34db59576fe3bae1d8537fa3d
yuriiburov <burovyuriireact@gmail.com>
2022-02-17 09:15:59 +0200

    init

--- lesson2_redux-intro/01-task/src/index.js
+++ lesson2_redux-intro/01-task/src/index.js
@@ -3,19 +3,19 @@ import { createStore } from 'redux';
 const INCREMENT = 'COUNTER/INCREMENT';
 const DECREMENT = 'COUNTER/DECREMENT';
 
-const increment = () => {
+export const increment = () => {
   return {
     type: INCREMENT,
   };
 };
 
-const decrement = () => {
+export const decrement = () => {
   return {
     type: DECREMENT,
   };
 };
 
-const counterReducer = (state = 0, action) => {
+export const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
@@ -26,4 +26,4 @@ const counterReducer = (state = 0, action) => {
   }
 };
 
-const store = createStore(counterReducer);
+export const store = createStore(counterReducer);
